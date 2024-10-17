cebf53b05431f1f4c67fc016d2fa23ca7dd2cf9e
unknown <yiiipunn@gmail.com>
2024-10-18 04:06:53 +0700

    i'm done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >
+      <body className="scroll-smooth" >
         <Navbar/>
         {children}
       </body>
