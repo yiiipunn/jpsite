4fca6103f5b73483fa4bc8c6d882fb12e6606049
unknown <yiiipunn@gmail.com>
2024-10-18 00:42:26 +0700

    timeline:updates + descript:updates

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -2,9 +2,9 @@
 import React from "react";
 
 const Education = () => (
-  <div className="container lg:px-50 lg:pt-50 md:ml-20 sm:ml-20 mx-auto z-4">
+  <div className="container lg:px-50 lg:pt-50 md:ml-20  mx-auto z-4">
     <div className="md:w-2/3">
-      <h1 className="font-bold italic mb-10 pt-10 text-3xl  text-sky-900">
+      <h1 className="font-bold italic mb-10 pt-10 text-3xl text-sky-900">
         Education
       </h1>
 
