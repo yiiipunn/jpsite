273c857be4ab23c4ae58d3c1abb749def5643ee5
unknown <yiiipunn@gmail.com>
2024-10-18 02:57:02 +0700

    almost done!

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,16 +4,22 @@ import React from "react";
 const Skills = () => (
   <div className="container lg:px-50 lg:pt-50 mx-auto z-4" id="Skills">
     <div>
-    <h1 className="font-bold italic  py-8  text-3xl text-sky-900">
-        Skills
-      </h1>
-      <a href="#" className="block max-w-sm p-6 bg-white border rounded-lg shadow border-gray-700">
-<p className="font-semibold text-gray-700 dark:text-gray-500">&#8226; UX/UI Designer<br/>
-&#8226; Front-End Developer<br/>
-  &#8226; Photographer / FilmMaker<br/>
-&#8226; Electric Guitarist<br/>
-</p>
-</a>
+      <h1 className="font-bold italic  py-8  text-3xl text-sky-900">Skills</h1>
+      <a
+        href="#"
+        className="block max-w-sm p-6 bg-white border rounded-lg shadow border-gray-700"
+      >
+        <p className="font-semibold text-gray-700 dark:text-gray-500">
+          &#8226; UX/UI Designer
+          <br />
+          &#8226; Front-End Developer
+          <br />
+          &#8226; Photographer / FilmMaker
+          <br />
+          &#8226; Electric Guitarist
+          <br />
+        </p>
+      </a>
     </div>
   </div>
 );
