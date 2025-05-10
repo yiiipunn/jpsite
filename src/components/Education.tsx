bc69e642346277409581a239a2aae6740053f45b
unknown <yiiipunn@gmail.com>
2025-05-11 00:02:10 +0700

    fixed '&apos;'

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Project from "./Project";
 import { motion } from "framer-motion";
 
 const fadeInLeft = {
